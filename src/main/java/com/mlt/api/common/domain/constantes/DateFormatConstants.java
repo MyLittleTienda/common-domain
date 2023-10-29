081c154060fb99a64e23607f4852c7366630806e
RustyCrazyPunky <crazypunky@protonmail.com>
2023-10-29 13:42:33 -0300

    Update for GCP

--- src/main/java/com/mlt/api/common/domain/constantes/DateFormatConstants.java
+++ src/main/java/com/mlt/api/common/domain/constantes/DateFormatConstants.java
@@ -2,6 +2,7 @@ package com.mlt.api.common.domain.constantes;
 
 public class DateFormatConstants {
     public static final String DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+    public static final String DATE_FORMAT = "yyyy-MM-dd";
 
     private DateFormatConstants() {
     }
